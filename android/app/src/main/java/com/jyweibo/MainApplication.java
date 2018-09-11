ccd0efddb2289424381e3f66153f99e2fd91954f
HFY <1440779243@qq.com>
2018-09-11 18:13:01 +0800

    change

--- android/app/src/main/java/com/jyweibo/MainApplication.java
+++ android/app/src/main/java/com/jyweibo/MainApplication.java
@@ -7,8 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-
+import com.umeng.commonsdk.UMConfigure;
 import java.util.Arrays;
 import java.util.List;
 
@@ -45,6 +44,8 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-
+    UMConfigure.init(this,"5b974b458f4a9d622c000173"
+            ,"umeng",UMConfigure.DEVICE_TYPE_PHONE,"");
+//    PlatformConfig.setSinaWeibo("3414425297", "fc2b4e5cf093bc02c9fac20583dbdd0e","https://www.jianshu.com/");
   }
 }
